259d8e99a38201cf345dc49234f7e1b3f228970d
Stargazerxiii-design <78391698+Tridecabase@users.noreply.github.com>
2024-10-31 17:00:12 +0900

    Update player.h

--- TD/Characters/player.h
+++ TD/Characters/player.h
@@ -17,6 +17,21 @@ public:
 	//プレイヤーの描画
 	void Draw(Map* map);
 
+	/*他のcppにplayer.cppの情報を渡す関数
+	*例：
+	* stage.cppの中
+	* 宣言の時:
+	* Vector2 playerSpeed;
+	* 初期化の時:
+	* playerSpeed = {};
+	* 更新処理の時(Stage::Update()の中):
+	* playerSpeed = player->GetPlayerSpeed();
+	*/
+	//プレイヤーの位置ベクトル
+	Vector2 GetPlayerPos() const { return pos; }
+	//プレイヤーの速度ベクトル
+	Vector2 GetPlayerSpeed() const { return speed; }
+
 private:
 
 	// ============================
