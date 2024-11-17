fc8f528f072319da514f6767ffba24d29add2f5a
keno555552 <kwankinleung5@gmail.com>
2024-11-17 22:33:13 +0900

    絵描き関数の更新とプレイヤーステージ更新
    詳しくはdrawPolygon.classを見て

--- TD/Scene/background.cpp
+++ TD/Scene/background.cpp
@@ -2,71 +2,178 @@
 #include <memory>
 
 PlayerRoad::PlayerRoad() {
-	if(true){
-		float aa = 37.0f;
-		float ab = 127.0f;
-		float ba = 58.0f;
-		float bb = 180.0f;
+	{
+		///横線用
+		float y1 = 530.0f;
+		float y2 = 572.0f;
+		float y3 = 620.0f;
+		float y4 = 682.0f;
+
+		///縦線用
+		float a1 = 45.0f;
+		float a2 = 130.0f;
+		float b1 = 63.0f;
+		float b2 = 195.0f;
 
 		///横線
-		Line1P1 = { -1.0f, 530.0f };
-		Line1P2 = { 1281.0f, 530.0f };
-		Line2P1 = { -1.0f, 570.0f };
-		Line2P2 = { 1281.0f, 570.0f };
-		Line3P1 = { -1.0f, 618.0f };
-		Line3P2 = { 1281.0f, 618.0f };
-		Line4P1 = { -1.0f, 672.0f };
-		Line4P2 = { 1281.0f, 672.0f };
+		Line1P1 = { -1.0f, y1 };
+		Line1P2 = { 1281.0f, y1 };
+		Line2P1 = { -1.0f, y2 };
+		Line2P2 = { 1281.0f, y2 };
+		Line3P1 = { -1.0f, y3 };
+		Line3P2 = { 1281.0f, y3 };
+		Line4P1 = { -1.0f, y4 };
+		Line4P2 = { 1281.0f, y4 };
 		///縦線
-		Line5P1 = { 640.0f - ab, 530.0f };
-		Line5P2 = { 640.0f - bb, 672.0f };
-		Line6P1 = { 640.0f - aa, 530.0f };
-		Line6P2 = { 640.0f - ba, 672.0f };
-		Line7P1 = { 640.0f + aa, 530.0f };
-		Line7P2 = { 640.0f + ba, 672.0f };
-		Line8P1 = { 640.0f + ab, 530.0f };
-		Line8P2 = { 640.0f + bb, 672.0f };
-	}
+		Line5P1 = { 640.0f - a2, y1 };
+		Line5P2 = { 640.0f - b2, y4 };
+		Line6P1 = { 640.0f - a1, y1 };
+		Line6P2 = { 640.0f - b1, y4 };
+		Line7P1 = { 640.0f + a1, y1 };
+		Line7P2 = { 640.0f + b1, y4 };
+		Line8P1 = { 640.0f + a2, y1 };
+		Line8P2 = { 640.0f + b2, y4 };
 
-	if (true) {
-		float a1 = 70.0f;
-		float a2 = 100.0f;
-		float b1 = 100.0f;
-		float b2 = 130.0f;
-		float c1 = 80.0f;
-		float c2 = 110.0f;
+		///矢印
+		float c1 = 200.0f;
+		float c2 = 170.0f;
+		float d1 = 280.0f;
+		float d2 = 250.0f;
+		float e1 = 270.0f;
+		float e2 = 240.0f;
 
-		ArrowLeftPoint1 = {640.0f - a1, 530.0f};
-		ArrowLeftPoint2 = {640.0f - a2, 530.0f};
-		ArrowLeftPoint3 = {640.0f - b1, 601.0f};
-		ArrowLeftPoint4 = {640.0f - b2, 601.0f};
-		ArrowLeftPoint5 = {640.0f - c1, 672.0f};
-		ArrowLeftPoint6 = {640.0f - c2, 672.0f};
+		///左
+		ArrowLeftPoint1 = { 640.0f - c1, y1 };
+		ArrowLeftPoint2 = { 640.0f - c2, y1 };
+		ArrowLeftPoint3 = { 640.0f - d1, 595.0f };
+		ArrowLeftPoint4 = { 640.0f - d2, 595.0f };
+		ArrowLeftPoint5 = { 640.0f - e1, y4 };
+		ArrowLeftPoint6 = { 640.0f - e2, y4 };
+
+		///右
+		ArrowRightPoint1 = { 640.0f + c1, y1 };
+		ArrowRightPoint2 = { 640.0f + c2, y1 };
+		ArrowRightPoint3 = { 640.0f + d1, 595.0f };
+		ArrowRightPoint4 = { 640.0f + d2, 595.0f };
+		ArrowRightPoint5 = { 640.0f + e1, y4 };
+		ArrowRightPoint6 = { 640.0f + e2, y4 };
 	}
 
 };
 
-void PlayerRoad::render(int color) {
+void PlayerRoad::render(Player* player, int color) {
 	///test
-	Novice::DrawLine(640, 0,640, 720, color);
+	Novice::DrawLine(640, 0, 640, 720, color);
 	///横線
 	Novice::DrawLine(int(Line1P1.x), int(Line1P1.y), int(Line1P2.x), int(Line1P2.y), color);
-	Novice::DrawLine(int(Line2P1.x), int(Line2P1.y), int(Line2P2.x), int(Line2P2.y), color);
-	Novice::DrawLine(int(Line3P1.x), int(Line3P1.y), int(Line3P2.x), int(Line3P2.y), color);
-	Novice::DrawLine(int(Line4P1.x), int(Line4P1.y), int(Line4P2.x), int(Line4P2.y), color);
+	//Novice::DrawLine(int(Line2P1.x), int(Line2P1.y), int(Line2P2.x), int(Line2P2.y), color);
+	//Novice::DrawLine(int(Line3P1.x), int(Line3P1.y), int(Line3P2.x), int(Line3P2.y), color);
+	//Novice::DrawLine(int(Line4P1.x), int(Line4P1.y), int(Line4P2.x), int(Line4P2.y), color);
+	DrawLineNew(Line2P1, Line2P2, 3, color);
+	DrawLineNew(Line3P1, Line3P2, 5, color);
+	DrawLineNew(Line4P1, Line4P2, 7, color);
+
 	///縦線
 	Novice::DrawLine(int(Line5P1.x), int(Line5P1.y), int(Line5P2.x), int(Line5P2.y), color);
+	Novice::DrawLine(int(Line5P1.x), int(Line5P1.y), int(Line5P2.x + 1), int(Line5P2.y), color);
+	Novice::DrawLine(int(Line5P1.x), int(Line5P1.y), int(Line5P2.x - 1), int(Line5P2.y), color);
+	Novice::DrawLine(int(Line5P1.x), int(Line5P1.y), int(Line5P2.x + 2), int(Line5P2.y), color);
+	Novice::DrawLine(int(Line5P1.x), int(Line5P1.y), int(Line5P2.x - 2), int(Line5P2.y), color);
 	Novice::DrawLine(int(Line6P1.x), int(Line6P1.y), int(Line6P2.x), int(Line6P2.y), color);
+	Novice::DrawLine(int(Line6P1.x), int(Line6P1.y), int(Line6P2.x + 1), int(Line6P2.y), color);
+	Novice::DrawLine(int(Line6P1.x), int(Line6P1.y), int(Line6P2.x - 1), int(Line6P2.y), color);
+	Novice::DrawLine(int(Line6P1.x), int(Line6P1.y), int(Line6P2.x + 2), int(Line6P2.y), color);
+	Novice::DrawLine(int(Line6P1.x), int(Line6P1.y), int(Line6P2.x - 2), int(Line6P2.y), color);
 	Novice::DrawLine(int(Line7P1.x), int(Line7P1.y), int(Line7P2.x), int(Line7P2.y), color);
+	Novice::DrawLine(int(Line7P1.x), int(Line7P1.y), int(Line7P2.x + 1), int(Line7P2.y), color);
+	Novice::DrawLine(int(Line7P1.x), int(Line7P1.y), int(Line7P2.x - 1), int(Line7P2.y), color);
+	Novice::DrawLine(int(Line7P1.x), int(Line7P1.y), int(Line7P2.x + 2), int(Line7P2.y), color);
+	Novice::DrawLine(int(Line7P1.x), int(Line7P1.y), int(Line7P2.x - 2), int(Line7P2.y), color);
 	Novice::DrawLine(int(Line8P1.x), int(Line8P1.y), int(Line8P2.x), int(Line8P2.y), color);
+	Novice::DrawLine(int(Line8P1.x), int(Line8P1.y), int(Line8P2.x + 1), int(Line8P2.y), color);
+	Novice::DrawLine(int(Line8P1.x), int(Line8P1.y), int(Line8P2.x - 1), int(Line8P2.y), color);
+	Novice::DrawLine(int(Line8P1.x), int(Line8P1.y), int(Line8P2.x + 2), int(Line8P2.y), color);
+	Novice::DrawLine(int(Line8P1.x), int(Line8P1.y), int(Line8P2.x - 2), int(Line8P2.y), color);
+
 
 	///矢印(左)
+	if (player->isPlayerLeft) {
+		DrawPolygon(ArrowLeftPoint1, ArrowLeftPoint2, ArrowLeftPoint3, ArrowLeftPoint4, ArrowLeftPoint5, ArrowLeftPoint6, color);
+	}
 	Novice::DrawLine(int(ArrowLeftPoint1.x), int(ArrowLeftPoint1.y), int(ArrowLeftPoint2.x), int(ArrowLeftPoint2.y), color);
 	Novice::DrawLine(int(ArrowLeftPoint2.x), int(ArrowLeftPoint2.y), int(ArrowLeftPoint4.x), int(ArrowLeftPoint4.y), color);
 	Novice::DrawLine(int(ArrowLeftPoint4.x), int(ArrowLeftPoint4.y), int(ArrowLeftPoint6.x), int(ArrowLeftPoint6.y), color);
 	Novice::DrawLine(int(ArrowLeftPoint6.x), int(ArrowLeftPoint6.y), int(ArrowLeftPoint5.x), int(ArrowLeftPoint5.y), color);
 	Novice::DrawLine(int(ArrowLeftPoint5.x), int(ArrowLeftPoint5.y), int(ArrowLeftPoint3.x), int(ArrowLeftPoint3.y), color);
 	Novice::DrawLine(int(ArrowLeftPoint3.x), int(ArrowLeftPoint3.y), int(ArrowLeftPoint1.x), int(ArrowLeftPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowLeftPoint1.x + 1), int(ArrowLeftPoint1.y), int(ArrowLeftPoint2.x + 1), int(ArrowLeftPoint2.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint2.x + 1), int(ArrowLeftPoint2.y), int(ArrowLeftPoint4.x + 1), int(ArrowLeftPoint4.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint4.x + 1), int(ArrowLeftPoint4.y), int(ArrowLeftPoint6.x + 1), int(ArrowLeftPoint6.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint6.x + 1), int(ArrowLeftPoint6.y), int(ArrowLeftPoint5.x + 1), int(ArrowLeftPoint5.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint5.x + 1), int(ArrowLeftPoint5.y), int(ArrowLeftPoint3.x + 1), int(ArrowLeftPoint3.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint3.x + 1), int(ArrowLeftPoint3.y), int(ArrowLeftPoint1.x + 1), int(ArrowLeftPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowLeftPoint1.x + 2), int(ArrowLeftPoint1.y), int(ArrowLeftPoint2.x + 2), int(ArrowLeftPoint2.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint2.x + 2), int(ArrowLeftPoint2.y), int(ArrowLeftPoint4.x + 2), int(ArrowLeftPoint4.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint4.x + 2), int(ArrowLeftPoint4.y), int(ArrowLeftPoint6.x + 2), int(ArrowLeftPoint6.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint6.x + 2), int(ArrowLeftPoint6.y), int(ArrowLeftPoint5.x + 2), int(ArrowLeftPoint5.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint5.x + 2), int(ArrowLeftPoint5.y), int(ArrowLeftPoint3.x + 2), int(ArrowLeftPoint3.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint3.x + 2), int(ArrowLeftPoint3.y), int(ArrowLeftPoint1.x + 2), int(ArrowLeftPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowLeftPoint1.x - 1), int(ArrowLeftPoint1.y), int(ArrowLeftPoint2.x - 1), int(ArrowLeftPoint2.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint2.x - 1), int(ArrowLeftPoint2.y), int(ArrowLeftPoint4.x - 1), int(ArrowLeftPoint4.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint4.x - 1), int(ArrowLeftPoint4.y), int(ArrowLeftPoint6.x - 1), int(ArrowLeftPoint6.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint6.x - 1), int(ArrowLeftPoint6.y), int(ArrowLeftPoint5.x - 1), int(ArrowLeftPoint5.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint5.x - 1), int(ArrowLeftPoint5.y), int(ArrowLeftPoint3.x - 1), int(ArrowLeftPoint3.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint3.x - 1), int(ArrowLeftPoint3.y), int(ArrowLeftPoint1.x - 1), int(ArrowLeftPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowLeftPoint1.x - 2), int(ArrowLeftPoint1.y), int(ArrowLeftPoint2.x - 2), int(ArrowLeftPoint2.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint2.x - 2), int(ArrowLeftPoint2.y), int(ArrowLeftPoint4.x - 2), int(ArrowLeftPoint4.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint4.x - 2), int(ArrowLeftPoint4.y), int(ArrowLeftPoint6.x - 2), int(ArrowLeftPoint6.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint6.x - 2), int(ArrowLeftPoint6.y), int(ArrowLeftPoint5.x - 2), int(ArrowLeftPoint5.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint5.x - 2), int(ArrowLeftPoint5.y), int(ArrowLeftPoint3.x - 2), int(ArrowLeftPoint3.y), color);
+	Novice::DrawLine(int(ArrowLeftPoint3.x - 2), int(ArrowLeftPoint3.y), int(ArrowLeftPoint1.x - 2), int(ArrowLeftPoint1.y), color);
+
+
+	///矢印(左)
+	if (player->isPlayerRight) {
+		DrawPolygon(ArrowRightPoint1, ArrowRightPoint2, ArrowRightPoint3, ArrowRightPoint4, ArrowRightPoint5, ArrowRightPoint6, color);
+	}
+	Novice::DrawLine(int(ArrowRightPoint1.x), int(ArrowRightPoint1.y), int(ArrowRightPoint2.x), int(ArrowRightPoint2.y), color);
+	Novice::DrawLine(int(ArrowRightPoint2.x), int(ArrowRightPoint2.y), int(ArrowRightPoint4.x), int(ArrowRightPoint4.y), color);
+	Novice::DrawLine(int(ArrowRightPoint4.x), int(ArrowRightPoint4.y), int(ArrowRightPoint6.x), int(ArrowRightPoint6.y), color);
+	Novice::DrawLine(int(ArrowRightPoint6.x), int(ArrowRightPoint6.y), int(ArrowRightPoint5.x), int(ArrowRightPoint5.y), color);
+	Novice::DrawLine(int(ArrowRightPoint5.x), int(ArrowRightPoint5.y), int(ArrowRightPoint3.x), int(ArrowRightPoint3.y), color);
+	Novice::DrawLine(int(ArrowRightPoint3.x), int(ArrowRightPoint3.y), int(ArrowRightPoint1.x), int(ArrowRightPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowRightPoint1.x + 1), int(ArrowRightPoint1.y), int(ArrowRightPoint2.x + 1), int(ArrowRightPoint2.y), color);
+	Novice::DrawLine(int(ArrowRightPoint2.x + 1), int(ArrowRightPoint2.y), int(ArrowRightPoint4.x + 1), int(ArrowRightPoint4.y), color);
+	Novice::DrawLine(int(ArrowRightPoint4.x + 1), int(ArrowRightPoint4.y), int(ArrowRightPoint6.x + 1), int(ArrowRightPoint6.y), color);
+	Novice::DrawLine(int(ArrowRightPoint6.x + 1), int(ArrowRightPoint6.y), int(ArrowRightPoint5.x + 1), int(ArrowRightPoint5.y), color);
+	Novice::DrawLine(int(ArrowRightPoint5.x + 1), int(ArrowRightPoint5.y), int(ArrowRightPoint3.x + 1), int(ArrowRightPoint3.y), color);
+	Novice::DrawLine(int(ArrowRightPoint3.x + 1), int(ArrowRightPoint3.y), int(ArrowRightPoint1.x + 1), int(ArrowRightPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowRightPoint1.x + 2), int(ArrowRightPoint1.y), int(ArrowRightPoint2.x + 2), int(ArrowRightPoint2.y), color);
+	Novice::DrawLine(int(ArrowRightPoint2.x + 2), int(ArrowRightPoint2.y), int(ArrowRightPoint4.x + 2), int(ArrowRightPoint4.y), color);
+	Novice::DrawLine(int(ArrowRightPoint4.x + 2), int(ArrowRightPoint4.y), int(ArrowRightPoint6.x + 2), int(ArrowRightPoint6.y), color);
+	Novice::DrawLine(int(ArrowRightPoint6.x + 2), int(ArrowRightPoint6.y), int(ArrowRightPoint5.x + 2), int(ArrowRightPoint5.y), color);
+	Novice::DrawLine(int(ArrowRightPoint5.x + 2), int(ArrowRightPoint5.y), int(ArrowRightPoint3.x + 2), int(ArrowRightPoint3.y), color);
+	Novice::DrawLine(int(ArrowRightPoint3.x + 2), int(ArrowRightPoint3.y), int(ArrowRightPoint1.x + 2), int(ArrowRightPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowRightPoint1.x - 1), int(ArrowRightPoint1.y), int(ArrowRightPoint2.x - 1), int(ArrowRightPoint2.y), color);
+	Novice::DrawLine(int(ArrowRightPoint2.x - 1), int(ArrowRightPoint2.y), int(ArrowRightPoint4.x - 1), int(ArrowRightPoint4.y), color);
+	Novice::DrawLine(int(ArrowRightPoint4.x - 1), int(ArrowRightPoint4.y), int(ArrowRightPoint6.x - 1), int(ArrowRightPoint6.y), color);
+	Novice::DrawLine(int(ArrowRightPoint6.x - 1), int(ArrowRightPoint6.y), int(ArrowRightPoint5.x - 1), int(ArrowRightPoint5.y), color);
+	Novice::DrawLine(int(ArrowRightPoint5.x - 1), int(ArrowRightPoint5.y), int(ArrowRightPoint3.x - 1), int(ArrowRightPoint3.y), color);
+	Novice::DrawLine(int(ArrowRightPoint3.x - 1), int(ArrowRightPoint3.y), int(ArrowRightPoint1.x - 1), int(ArrowRightPoint1.y), color);
+
+	Novice::DrawLine(int(ArrowRightPoint1.x - 2), int(ArrowRightPoint1.y), int(ArrowRightPoint2.x - 2), int(ArrowRightPoint2.y), color);
+	Novice::DrawLine(int(ArrowRightPoint2.x - 2), int(ArrowRightPoint2.y), int(ArrowRightPoint4.x - 2), int(ArrowRightPoint4.y), color);
+	Novice::DrawLine(int(ArrowRightPoint4.x - 2), int(ArrowRightPoint4.y), int(ArrowRightPoint6.x - 2), int(ArrowRightPoint6.y), color);
+	Novice::DrawLine(int(ArrowRightPoint6.x - 2), int(ArrowRightPoint6.y), int(ArrowRightPoint5.x - 2), int(ArrowRightPoint5.y), color);
+	Novice::DrawLine(int(ArrowRightPoint5.x - 2), int(ArrowRightPoint5.y), int(ArrowRightPoint3.x - 2), int(ArrowRightPoint3.y), color);
+	Novice::DrawLine(int(ArrowRightPoint3.x - 2), int(ArrowRightPoint3.y), int(ArrowRightPoint1.x - 2), int(ArrowRightPoint1.y), color);
 };
 
 
@@ -115,15 +222,13 @@ void Background::Update(Player* player, Map* map, char keys[256]) {
 		if (keys[DIK_A]) {
 			scrollX -= OUTER_BG_SPEED;
 			player->isPlayerLeft = true;
-		}
-		else {
+		} else {
 			player->isPlayerLeft = false;
 		}
 		if (keys[DIK_D]) {
 			scrollX += OUTER_BG_SPEED;
 			player->isPlayerRight = true;
-		}
-		else {
+		} else {
 			player->isPlayerRight = false;
 		}
 	}
@@ -131,7 +236,7 @@ void Background::Update(Player* player, Map* map, char keys[256]) {
 };
 
 //背景の描画処理
-void Background::Render() {
+void Background::Render(Player* player) {
 
 	for (int i = 0; i < 6; ++i) {
 		int drawX = offsetX + i * static_cast<int>(WINDOW_WIDTH);
@@ -142,7 +247,7 @@ void Background::Render() {
 		resourceManager.Draw(drawX, 0, textureName, 1.0f, 1.0f, 0.0f, 0xffffffff);
 	}
 
-	playerRoad->render(0xFFFFFFFF);
+	playerRoad->render(player, 0xFFFFFFFF);
 
 	Novice::ScreenPrintf(10, 60, "scollX %f", scrollX);
 };
---
+++ TD/drawPolygon.cpp
@@ -0,0 +1,68 @@
+﻿#include "drawPolygon.h"
+
+
+
+///////////////////////////////////////////////////////////////Polygon
+
+void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point3, const Vector2 point4,const int color) {
+	Novice::DrawTriangle(int(point1.x), int(point1.y),
+						 int(point2.x), int(point2.y),
+						 int(point3.x), int(point3.y),
+						 color, kFillModeSolid);
+	Novice::DrawTriangle(int(point1.x), int(point1.y),
+						 int(point3.x), int(point3.y),
+						 int(point4.x), int(point4.y),
+						 color, kFillModeSolid);
+}
+
+void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point3, const Vector2 point4
+			   , const Vector2 point5, const int color){
+	Novice::DrawTriangle(int(point1.x), int(point1.y),
+						 int(point2.x), int(point2.y),
+						 int(point3.x), int(point3.y),
+						 color, kFillModeSolid);
+	Novice::DrawTriangle(int(point2.x), int(point2.y),
+						 int(point3.x), int(point3.y),
+						 int(point4.x), int(point4.y),
+						 color, kFillModeSolid);
+	Novice::DrawTriangle(int(point3.x), int(point3.y),
+						 int(point4.x), int(point4.y),
+						 int(point5.x), int(point5.y),
+						 color, kFillModeSolid);
+}
+
+void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point3, const Vector2 point4
+			   , const Vector2 point5, const Vector2 point6, const int color){
+	Novice::DrawTriangle(int(point1.x), int(point1.y),
+						 int(point2.x), int(point2.y),
+						 int(point3.x), int(point3.y),
+						 color, kFillModeSolid);
+	Novice::DrawTriangle(int(point2.x), int(point2.y),
+						 int(point3.x), int(point3.y),
+						 int(point4.x), int(point4.y),
+						 color, kFillModeSolid);
+	Novice::DrawTriangle(int(point3.x), int(point3.y),
+						 int(point4.x), int(point4.y),
+						 int(point5.x), int(point5.y),
+						 color, kFillModeSolid);
+	Novice::DrawTriangle(int(point4.x), int(point4.y),
+						 int(point5.x), int(point5.y),
+						 int(point6.x), int(point6.y),
+						 color, kFillModeSolid);
+}
+
+
+///////////////////////////////////////////////////////////////Line
+
+void DrawLineNew(const Vector2 point1, const Vector2 point2, const int px,int color) {
+	for (int i = 0; i < px; i++) {
+		if ((point2.x - point1.x) > (point2.y - point1.y)) {
+			int a = int(powf(-1.0f, float(i)));
+			Novice::DrawLine(int(point1.x + (i / 2)), int(point1.y + a * (i / 2)), int(point2.x + (i / 2)), int(point1.y + a * (i / 2)), color);
+		} else {
+			int a = int(powf(-1.0f, float(i)));
+			Novice::DrawLine(int(point1.x + a * (i / 2)), int(point1.y + (i / 2)), int(point2.x + a * (i / 2)), int(point1.y + (i / 2)), color);
+		}
+	}
+
+};
