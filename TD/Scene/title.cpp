3fe155999ffc2f3cdd6a71f853077800c675d17d
keno555552 <kwankinleung5@gmail.com>
2024-11-22 07:38:22 +0900

    0と1を描く関数追加

--- TD/Scene/title.cpp
+++ TD/Scene/title.cpp
@@ -23,4 +23,8 @@ void Title::StageChanger(){
 
 void Title::Render() {
 	Novice::DrawBox(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0f,0x191b19FF, kFillModeSolid);
+
+	DrawNum(500, 400, 50, 0x4BBC54FF,0);
+	DrawNum(600, 400, 50, int(50 * 1.4f), 0x4BBC54FF, 5);
+	DrawNum(500, 600, 50, 0x4BBC54FF,1);
 }
--- TD/drawPolygon.h
+++ TD/drawPolygon.h
@@ -25,7 +25,7 @@ void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point
 /// <param name="point5">Vector2 point5</param>
 /// <param name="color"></param>
 void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point3, const Vector2 point4
-			   , const Vector2 point5, const int color);
+	, const Vector2 point5, const int color);
 
 /// <summary>
 /// 六角形塗りつぶしを描く
@@ -38,9 +38,32 @@ void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point
 /// <param name="point6">Vector2 point6</param>
 /// <param name="color"></param>
 void DrawPolygon(const Vector2 point1, const Vector2 point2, const Vector2 point3, const Vector2 point4
-			   , const Vector2 point5, const Vector2 point6, const int color);
+	, const Vector2 point5, const Vector2 point6, const int color);
 
 ///////////////////////////////////////////////////////////////Line
 
 
 void DrawLineNew(const Vector2 point1, const Vector2 point2, const int px, int color);
+
+///////////////////////////////////////////////////////////////0/1
+
+/// <summary>
+/// 0を描く
+/// </summary>
+/// <param name="posX">描きたい0の中心点posX</param>
+/// <param name="posY">描きたい0の中心点posY</param>
+/// <param name="width">描きたい0の横幅</param>
+/// <param name="height">描きたい0の縦幅</param>
+/// <param name="color">描きたい色</param>
+/// <param name="Num">描きたい数字、今は0が1しができない</param>
+void DrawNum(const int posX, const int posY, const int width, const int height, const int color, int Num);
+
+/// <summary>
+/// 0を描く、箱は正方形
+/// </summary>
+/// <param name="posX">描きたい0の中心点posX</param>
+/// <param name="posY">描きたい0の中心点posY</param>
+/// <param name="width">描きたい0の横幅</param>
+/// <param name="color">描きたい色</param>
+/// <param name="Num">描きたい数字、今は0が1しができない</param>
+void DrawNum(const int posX, const int posY, const int width, const int color, int Num);
